3d2eddbab0ac6014316e56d3e694688be45cf673
Orr Matzkin <orr.matzkin@gmail.com>
2022-03-01 22:17:05 +0200

    added the run method for the jukebox

--- jukebox.py
+++ jukebox.py
@@ -1,3 +1,4 @@
+from asyncio import FastChildWatcher
 import sys
 from typing import List
 from urllib import request
@@ -9,6 +10,7 @@ from Adafruit_IO import MQTTClient, Client, Feed, RequestError
 
 ADAFRUIT_IO_KEY = "<YOUR ADAFRUIT IO KEY>"
 ADAFRUIT_IO_USERNAME = "<YOUR ADAFRUIT IO USERNAME>"
+AIO_FEED_ID = 'playing-song'
 
 class Jukebox:
 
@@ -24,11 +26,19 @@ class Jukebox:
             songs = json.load(f)
         self.songs_by_name = dict([(s['name'], s) for s in songs])   
 
-        self.vlc_instance = vlc.Instance()
+        self.vlc_instance = vlc.Instance('--quiet')
         self.media_player = self.vlc_instance.media_player_new()  
         self.media_player.toggle_fullscreen()
 
-    def play_video(self, song_name: str) -> bool:
+        self.aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+        self.feed = self.aio.feeds(AIO_FEED_ID)
+
+        self.is_playing = False
+        
+
+
+
+    def play_video(self, song_reqest: str) -> bool:
         """Playes the given song.
 
         Args:
@@ -37,15 +47,21 @@ class Jukebox:
         Returns:
             bool: True if jukebox is playing the song successfully, else False.    
         """
-        self.current_song = self.find_best_match(song_name)
-        if self.current_song is None:
+        song = self.find_best_match(song_reqest)
+        if song is None:
             print("song not found!")
             return False
-        else:    
+        elif song is not self.current_song:    
+            self.current_song = song
             print(f"playing {self.current_song['name']}")
             self.media_player.set_media(self.vlc_instance.media_new(self.current_song['path'])) 
             self.media_player.play()
+            self.is_playing = True
             return True
+        else:
+            print(f"{self.current_song['name']} is already playing")
+            return True
+
 
     def stop_video(self) -> None:
         """Stops the current music video, (if there is non playing does nothing).
@@ -53,7 +69,7 @@ class Jukebox:
         print("stoping music")
         self.current_song = None
         self.media_player.stop()
-
+        self.is_playing = False
 
     def find_best_match(self, reqest: str) -> List[str]:
         """Finds the best match of all song.
@@ -66,14 +82,32 @@ class Jukebox:
         """
         best_score = 0
         best_match = None
-        reqest_list = [s.lower() for s in reqest.split()]
+        reqest_list = {s.lower() for s in reqest.split()}
         for song in self.songs_by_name.values():
-            num_of_mathces = sum(x == y for x, y in zip(reqest_list, song["mathches"]))
-            score = num_of_mathces / len(song["mathches"])
+            num_of_mathces = len(reqest_list.intersection(song["matches"]))
+            score = num_of_mathces / len(song["matches"])
+            print(reqest_list)
+            print(song["matches"])
             if score > best_score:
                 best_score = score
                 best_match = song
         return best_match
+
+    def run(self) -> None:
+        old_reqest = None
+        while(True):
+            feed_data = self.aio.receive(self.feed.key)
+            song_reqest = feed_data.value
+            if song_reqest == "Null" and self.is_playing:
+                self.stop_video()    
+            elif song_reqest != old_reqest:
+                self.play_video(song_reqest)
+                old_reqest = song_reqest
+                
+            
+                
+                
+
 
 def io_test():
 
@@ -103,12 +137,15 @@ if __name__ == "__main__":
     # TODO: remove key and user name getters for final commit
     ADAFRUIT_IO_KEY = sys.argv[1]
     ADAFRUIT_IO_USERNAME = sys.argv[2]    
+    AIO_FEED_ID = sys.argv[3]
     
     jukebox = Jukebox("songs_data.json")
 
-    jukebox.play_video("YOu got")
-    time.sleep(5)
-    jukebox.stop_video()
+    jukebox.run()
+
+    # jukebox.play_video("the police")
+    # time.sleep(5)
+    # jukebox.stop_video()
 
     
 
